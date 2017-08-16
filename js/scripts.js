2ed2f46cdd98a792f88ae1af0afc90dc65353477
Evelyn Fenner-Dorrity and snow vilay <snow@>
2017-08-16 09:53:17 -0700

    add initial template for user to input sentence or word to be translated to pig latin, not yet functional

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#pig-latin-form").submit(function(event){
+    event.preventDefault();
+
+    var sentenceInput = $("#sentence").val();
+    var vowelCounter = 0;
+    //console.log(sentenceInput);
+
+    for(var i = 0; i <= sentenceInput.length - 1; i++){
+      if(sentenceInput)
+    }
+
+  });
+});
